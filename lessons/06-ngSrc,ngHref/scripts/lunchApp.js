d2f40603dac4130f24d6bc5ef3f956aa4bdc9d60
simalexan <alexander.simovic@gmail.com>
2013-10-18 15:50:10 +0200

    06 - ngSrc, ngHref ~ changes in the class ~ switching views

--- lessons/06-ngSrc,ngHref/scripts/lunchApp.js
+++ lessons/06-ngSrc,ngHref/scripts/lunchApp.js
@@ -5,5 +5,6 @@ var lunchApp = angular.module('lunchApp', ['ngRoute'])
     .config(function ($routeProvider){
         $routeProvider
             .when('/restaurants', {templateUrl: 'views/restaurants.html', controller: 'RestaurantCtrl'})
+            .when('/restaurant/:id', {templateUrl: 'views/restaurant.html', controller: 'RestaurantCtrl'})
             .otherwise({redirectTo: '/restaurants'});
     });
